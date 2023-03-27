0db7f9a008d28e55ec98f63615f844f0c3f6872c
Carlos Infante <kuva5008@gmail.com>
2023-03-27 10:09:50 +0200

    create database

---
+++ write_pdf.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+engine = create_engine("sqlite:///amet.db", echo=True)
+
+# SQL command to retrieve data
+comm1 = "SELECT * FROM paintings"
+comm2 = "SELECT * FROM customers"
+comm3 = "SELECT * FROM fans"
+
+# Read data from SQL using pandas
+df1 = pd.read_sql_query(comm1, con=engine)
+df2 = pd.read_sql_query(comm2, con=engine)
+df3 = pd.read_sql_query(comm3, con=engine)
+
+# Export data to excel
+with pd.ExcelWriter('Amet_data.xlsx') as writer:
+    df1.to_excel(writer, sheet_name='Paintings')
+    df2.to_excel(writer, sheet_name='Customers')
+    df3.to_excel(writer, sheet_name='Fans')
